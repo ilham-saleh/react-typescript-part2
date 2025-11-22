4ebc7507bc49d29d171ff11fa3a31a875fccb416
ilham saleh <ilham2saleh@gmail.com>
2025-11-22 10:32:39 +0000

    Refactor react query

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import todoService from "../services/todoService";
 import { CACHE_KEY_TODOS } from "../constants";
-import type { Todo } from "../services/todoService";
+import type { Todo } from "../entities/todoEntity";
+import ApiClient from "../services/apiClient";
+
+const APIClient = new ApiClient<Todo>("todos/");
 
 const useTodos = () => {
   // we can also customise the useQuery here if needed per query (globally in the main.tsx)
   const query = useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: todoService.getAll,
+    queryFn: APIClient.getAll,
     staleTime: 10 * 1000, // 10 seconds - customize stale time per query
   });
 
